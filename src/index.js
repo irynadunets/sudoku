8d2cf8a297f452d2736f3b09c972852f32b0bff5
Iryna Dunets <idunets@yahoo.com>
2020-03-21 22:18:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for(let i=0; i<matrix.length; i=i+1){
+    let missed = [];
+    let real = [];    
+    for(let j=1; j<10; j=j+1){ 
+      let findCount = matrix[i].filter(x => x===j).length;       
+      if(findCount<1) missed.push(j);
+    }   
+    for(let k=0; k<matrix.length; k=k+1){
+      if(matrix[i][k]===0||real.filter(x => x===matrix[i][k]).length>0){
+        matrix[i][k]=missed.shift();
+      } else {
+        real.push(matrix[i][k]);
+      }      
+    }      
+  }
+  return matrix;
 }
