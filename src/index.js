68db0ad51455678ecc4d74790d8b3303686d3e2e
Iryna Dunets <idunets@yahoo.com>
2020-03-22 18:42:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 module.exports = function solveSudoku(matrix) {
-  if(Fill(matrix)===true) return matrix;
-}
-
-function Check(matrix, i, j, k) {
-    for (let z = 0; z < matrix.length; z++) {
-        let m = 3 * Math.floor(i / 3) + Math.floor(i / 3);
-        let n = 3 * Math.floor(j / 3) + z % 3;
-        if (matrix[i][z] == k || matrix[z][j] == k || matrix[m][n] == k)  return false;
+  let check = (matrix, i, j, k) => {
+    for (let l = 0; l <  matrix.length; l++) {
+      if (matrix[i][l] == k || matrix[l][j] == k || matrix[3 * Math.floor(i / 3) + Math.floor(l / 3)][3 * Math.floor(j / 3) + l % 3] == k) return false;
       }
-    return true;
-  }
-
-function Fill(matrix) {
-  for(let i = 0; i < matrix.length; i++){
-    for(let j = 0; j < matrix.length; j++){
-      if(matrix[i][j] === 0) {
-        for (let k = 1; k <= matrix.length; k++) {
-          if (Check(matrix, i, j, k)) {
-            matrix[i][j] = k;
-          if(Fill(matrix)) {
-           return true;
-          } else {
-           matrix[i][j] = 0;
-          }
+      return true;
+    }
+  let Fill = (matrix) => {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j <  matrix.length; j++) {
+        if (matrix[i][j] === 0) {
+          for (let k = 1; k <=  matrix.length; k++) {
+            if (check(matrix, i, j, k)) {
+              matrix[i][j] = k;
+            if (Fill(matrix)) {
+             return true;
+            } else {
+             matrix[i][j] = 0;
+            }
+           }
          }
+         return false;
        }
-       return false;
      }
    }
- }
- return true;
+   return true;
+  }
+  if(Fill(matrix)) return matrix;
 }
-
