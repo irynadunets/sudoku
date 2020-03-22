34bc6a0c2cd73ae488e447e79f6e52b0f52c81f1
Iryna Dunets <idunets@yahoo.com>
2020-03-22 11:33:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 module.exports = function solveSudoku(matrix) {
-  if(Fill(matrix)) return matrix;
+  if(Fill(matrix)===true) return matrix;
 }
 
 function Check(matrix, i, j, k) {
-    for (let m = 0; m < matrix.length; m++) {        
-        if (matrix[i][m] == k || matrix[m][j] == k )  return false;
+    for (let z = 0; z < matrix.length; z++) {
+        let m = 3 * Math.floor(i / 3) + Math.floor(i / 3);
+        let n = 3 * Math.floor(j / 3) + z % 3;
+        if (matrix[i][z] == k || matrix[z][j] == k || matrix[m][n] == k)  return false;
       }
     return true;
   }
-  
+
 function Fill(matrix) {
   for(let i = 0; i < matrix.length; i++){
     for(let j = 0; j < matrix.length; j++){
